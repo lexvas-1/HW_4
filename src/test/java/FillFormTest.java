d55004e9fccbd55d128a5ef8b7819fdb4006fcde
lexvas-1 <lexsatellite@yandex.ru>
2024-07-13 17:21:57 +0300

    First commit create repo

---
+++ src/test/java/FillFormTest.java
@@ -0,0 +1,24 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class FillFormTest {
+
+    @BeforeAll
+static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void successfulFillFormTest() {
+
+        open("/automation-practice-form");
+
+
+    }
+}
