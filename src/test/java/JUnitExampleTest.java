5f33c6fe9877b1307eaef6abec6039a16d613abf
lexvas-1 <lexsatellite@yandex.ru>
2024-07-13 17:28:03 +0300

    Создан шаблон теста

--- src/test/java/JUnitExampleTest.java
+++ src/test/java/JUnitExampleTest.java
@@ -5,19 +5,19 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selenide.*;
 
 
-public class FillFormTest {
+public class JUnitExampleTest {
 
     @BeforeAll
 static void beforeAll() {
         Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.baseUrl = "https://github.com/";
         Configuration.pageLoadStrategy = "eager";
     }
 
     @Test
     void successfulFillFormTest() {
 
-        open("/automation-practice-form");
+        open("/selenide/selenide");
 
 
     }
